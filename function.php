1860a37277854df87345be638ec2d9cfabe118a0
Mohamed Zrida <zridamohamed96@gmail.com>
2023-10-09 21:51:11 -0500

    init

--- function.php
+++ function.php
@@ -23,7 +23,7 @@ elseif (preg_match('/[a-z]/', $mdp) && preg_match('/[A-Z]/', $mdp)) {
 return "Le niveau de mot de passe est passable, vous pouvez d'ajouter des chiffres. </br> MDP Salt: $mdpsalt, </br> Mot de passe chiffré: $mdpchiffré";
 } 
 
-elseif (preg_match('/[a-z]/', $mdp)|| preg_match('/[A-Z]/', $mdp))   {
+else  {
 return "Le niveau de mot de passe est faible. vous pouvez ajouter des lettres en majuscules ou minuscules et des chiffres, </br> Salt: $mdpsalt, </br> Mot de passe chiffré: $mdpchiffré";
 }
 }
